75c76512269b8e3e250af68486e6e1045059a583
Chinedum Roland Eke <47756447+rolandeke@users.noreply.github.com>
2023-02-02 09:40:01 +0000

    Create employees.js

---
+++ employees.js
@@ -0,0 +1,37 @@
+[
+  {
+    "employeeId": 1,
+    "name": "John Doe",
+    "department": "Marketing",
+    "email": "johndoe@company.com",
+    "phoneNumber": "+1 123 456 7890"
+  },
+  {
+    "employeeId": 2,
+    "name": "Jane Doe",
+    "department": "Sales",
+    "email": "janedoe@company.com",
+    "phoneNumber": "+1 987 654 3210"
+  },
+  {
+    "employeeId": 3,
+    "name": "Bob Smith",
+    "department": "IT",
+    "email": "bobsmith@company.com",
+    "phoneNumber": "+1 111 222 3333"
+  },
+  {
+    "employeeId": 4,
+    "name": "Sara Lee",
+    "department": "HR",
+    "email": "saralee@company.com",
+    "phoneNumber": "+1 555 666 7777"
+  },
+  {
+    "employeeId": 5,
+    "name": "Tom Davis",
+    "department": "Finance",
+    "email": "tomdavis@company.com",
+    "phoneNumber": "+1 999 888 1234"
+  }
+]
